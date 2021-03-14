8b22e51b78fa5271b330b1fdfdfb25664546cfc8
Sobhan Salimi <devgym.ir@gmail.com>
2021-03-14 22:10:56 +0330

    add gamer app

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>زمان کلیک - {{ score }} میلی ثانیه</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank : null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'عالی بود 💪🏻😍'
+    } else if (this.score < 400) {
+      this.rank = 'خوب بود  👍🏻'
+    } else {
+      this.rank = 'افتضاح 😂'
+    }
+  }
+}
+</script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>
